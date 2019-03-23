514f1e56820c3421ad839e87df51419b39674425
Do Duy Huy Hoang <huyhoang8398@gmail.com>
2019-03-23 19:54:55 +0700

    Remote Shell using raw socket with Python ver

--- Remote-Python/client.py
+++ Remote-Python/client.py
@@ -2,16 +2,16 @@ import socket
 
 def Main():
   host = '127.0.0.1'
-  port = 11000
-
-  s = socket.socket()
+  port = 61345
+  s = socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
+  # s = socket.socket()
   s.connect((host, port))
-  message = raw_input("-> ")
+  message = input("-> ")
   while message != 'q':
-    s.send(message)
+    s.send(message.encode('ascii'))     
     data = s.recv(1024)
-    print (data)
-    message = raw_input("-> ")
+    print (data.decode("utf-8"))
+    message = input("-> ")
   s.close
 
 if __name__ == '__main__':
--- Remote-Python/sever.py
+++ Remote-Python/sever.py
@@ -1,34 +1,62 @@
 import socket
+from _thread import *
+import threading 
+# thread fuction 
+# print_lock = threading.Lock()   
+def threaded(c): 
+    while True: 
+    # data received from client 
+      data = c.recv(1024) 
+      if not data: 
+        print('Bye') 
+          
+        # lock released on exit 
+        # print_lock.release() 
+        break
+      from subprocess import check_output
+      import subprocess
+        # subprocess.call(data)
+      process = subprocess.Popen(str(data.decode("utf-8")), stdout=subprocess.PIPE, stderr=None, shell=True)
 
-def Main():
-  host = '127.0.0.1'
-  port = 11000
+        #Launch the shell command:
+      output = process.communicate()
 
-  s = socket.socket()
-  s.bind((host, port))
-  s.listen(1)
-  c, addr = s.accept()
-  print ("Connection from:" + str(addr))
-  while True:
-    data = c.recv(1024)
-    if not data:
-      break
-    print ("from connected user:" +str(data))
-    from subprocess import check_output
-    import subprocess
-    # subprocess.call(data)
-    process = subprocess.Popen(str(data), stdout=subprocess.PIPE, stderr=None, shell=True)
+      print (output[0].decode("utf-8"))
+        # data = str(data).upper()
+        # out = check_output(str(data))
+        # print (str(out))
+        # print ("sending" + str(data))
+      c.send(output[0])
+# connection closed 
+    c.close() 
 
-    #Launch the shell command:
-    output = process.communicate()
-
-    print output[0]
-    # data = str(data).upper()
-    # out = check_output(str(data))
-    # print (str(out))
-    # print ("sending" + str(data))
-    c.send(output[0])
-  c.close()
+def Main(): 
+    host = "" 
+  
+    # reverse a port on your computer 
+    # in our case it is 12345 but it 
+    # can be anything 
+    port = 61345
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    s.bind((host, port)) 
+    print("socket binded to post", port) 
+  
+    # put the socket into listening mode 
+    s.listen(5) 
+    print("socket is listening") 
+  
+    # a forever loop until client wants to exit 
+    while True: 
+  
+        # establish connection with client 
+        c, addr = s.accept() 
+  
+        # lock acquired by client 
+        # print_lock.acquire() 
+        print('Connected to :', addr[0], ':', addr[1])
+        # Start a new thread and return its identifier 
+        start_new_thread(threaded, (c,)) 
+    s.close() 
 
 if __name__ == '__main__':
   Main()
